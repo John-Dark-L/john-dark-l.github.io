710c5b844556e3c146f91ffb6ada3c8ae02c285a
John-Dark-L <51474180+John-Dark-L@users.noreply.github.com>
2020-02-26 21:59:40 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,1 +1,21 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Loli-Coin</title>
+<center><h1>LoliCoin</h1></center>
+<link rel="stylesheet" type="text/css" href="style.css">
+<link rel="shortcut icon" href="icone.ico" type="image/x-icon" />
+</head>
 
+<body>
+<center>
+<img src="infinity.gif" width="300" height="150" alt=""/><br>
+<a href="videos.php">Fitas Aqui.</a><br>
+
+
+<img class="gun" src="imagem/borda.png" width="100" height="100"/><br>
+
+</center>
+</body>
+</html>
